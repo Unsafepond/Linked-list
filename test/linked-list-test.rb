096148dda4849daf67e508ec6396160f97defca2
Jerrod Junker <unsafepond@gmail.com>
2015-07-23 10:35:33 -0600

    even more tests

--- test/linked-list-test.rb
+++ test/linked-list-test.rb
@@ -3,6 +3,11 @@ require "../lib/linked-list"
 require "../lib/node"
 require 'pry'
 class TestLinkedList < Minitest::Test
+	def test_its_a_thing
+		list = LinkedList.new
+
+		assert list
+	end
 	def test_it_has_a_head
 		list = LinkedList.new
 		node = Node.new("1")
@@ -40,8 +45,18 @@ class TestLinkedList < Minitest::Test
 		result = list.head.next_node.next_node.next_node.next_node.data
 		assert_equal "4", result
 	end
+	def test_it_counts_2_nodes
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
 
-	def test_it_counts
+		list.append(node)
+		list.append(node2)
+		result = list.count
+		assert_equal 2, result
+	end
+
+	def test_it_counts_4_nodes
 		list = LinkedList.new
 		node = Node.new("1")
 		node2 = Node.new("2")
@@ -55,8 +70,58 @@ class TestLinkedList < Minitest::Test
 		result = list.count
 		assert_equal 4, result
 	end
+	def test_it_counts_5_nodes
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+		node4 = Node.new("4")
+		node5 = Node.new("5")
 
-	def test_it_finds_tail
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		list.append(node4)
+		list.append(node5)
+		result = list.count
+		assert_equal 5, result
+	end
+
+	def test_it_counts_6_nodes
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+		node4 = Node.new("4")
+		node5 = Node.new("5")
+		node6 = Node.new("6")
+
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		list.append(node4)
+		list.append(node5)
+		list.append(node6)
+		result = list.count
+		assert_equal 6, result
+	end
+
+	def test_it_finds_tail_with_3_nodes
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		result = list.find_tail.data
+		assert_equal "3", result
+	end
+
+	def test_it_finds_tail_with_4_nodes
 		list = LinkedList.new
 		node = Node.new("1")
 		node2 = Node.new("2")
@@ -71,7 +136,57 @@ class TestLinkedList < Minitest::Test
 		assert_equal "4", result
 	end
 
-	def test_it_pops
+	def test_it_finds_tail_with_5_nodes
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+		node4 = Node.new("4")
+		node5 = Node.new("5")
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		list.append(node4)
+		list.append(node5)
+		result = list.find_tail.data
+		assert_equal "5", result
+	end
+
+	def test_it_finds_tail_with_6_nodes
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+		node4 = Node.new("4")
+		node5 = Node.new("5")
+		node6 = Node.new("6")
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		list.append(node4)
+		list.append(node5)
+		list.append(node6)
+		result = list.find_tail.data
+		assert_equal "6", result
+	end
+
+	def test_it_pops_with_3
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		list.pop
+		result = list.find_tail.data
+		assert_equal "2", result
+	end
+
+	def test_it_pops_with_4
 		list = LinkedList.new
 		node = Node.new("1")
 		node2 = Node.new("2")
@@ -83,8 +198,65 @@ class TestLinkedList < Minitest::Test
 		list.append(node3)
 		list.append(node4)
 		list.pop
-		result = list.head.next_node.next_node.next_node.data
+		result = list.find_tail.data
 		assert_equal "3", result
+	end
+
+	def test_it_pops_with_5
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+		node4 = Node.new("4")
+		node5 = Node.new("5")
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		list.append(node4)
+		list.append(node5)
+		list.pop
+		result = list.find_tail.data
+		assert_equal "4", result
+	end
+
+	def test_it_pops_2_nodes
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+		node4 = Node.new("4")
+		node5 = Node.new("5")
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		list.append(node4)
+		list.append(node5)
+		list.pop
+		list.pop
+		result = list.find_tail.data
+		assert_equal "3", result
+	end
+
+	def test_it_pops_3_nodes
+		list = LinkedList.new
+		node = Node.new("1")
+		node2 = Node.new("2")
+		node3 = Node.new("3")
+		node4 = Node.new("4")
+		node5 = Node.new("5")
+
+		list.append(node)
+		list.append(node2)
+		list.append(node3)
+		list.append(node4)
+		list.append(node5)
+		list.pop
+		list.pop
+		list.pop
+		result = list.find_tail.data
+		assert_equal "2", result
 	end
 
 	def test_find_by_position
