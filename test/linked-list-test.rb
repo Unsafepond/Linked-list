75fe837b99cface500315f563daa1f76caa1289c
Jerrod Junker <unsafepond@gmail.com>
2015-07-23 10:43:08 -0600

    tests on tests on tests

--- test/linked-list-test.rb
+++ test/linked-list-test.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "minitest/pride"
 require "../lib/linked-list"
 require "../lib/node"
 require 'pry'
--- test/node-test.rb
+++ test/node-test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'minitest/pride'
 require '../lib/node'
 
 class NodeTest < Minitest::Test
@@ -20,9 +21,28 @@ class NodeTest < Minitest::Test
 		assert_equal nil, node.next_node
 	end
 
+	def test_next_node_can_be_broken
+		node = Node.new(1)
+		node2 = Node.new(2)
+		node3 = Node.new(3)
+
+		node.next_node = node2
+		node2.next_node = node3
+		node.next_node = node3
+
+		result = node.next_node.data
+		assert 3, result
+	end
+
 	def test_it_has_data
 		node = Node.new(1)
 		assert_equal 1, node.data
+	end
+
+	def test_you_can_change_data
+		node = Node.new(1)
+		node.data = 2
+		refute_equal 1, node.data
 	end
 
 	def test_it_has_a_position
